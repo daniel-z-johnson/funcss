6ad4ca4374d15980e8482ae03776f2d21b4426ee
daniel-z-johnson <danzaljohnson@gmail.com>
2020-09-29 20:52:31 -0600

    some db stuff

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/daniel-z-johnson/funcss
 
 go 1.15
 
-require github.com/jackc/pgx/v4 v4.8.1 // indirect
+require (
+	github.com/google/uuid v1.1.2
+	github.com/jackc/pgx/v4 v4.9.0
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
--- models/funcss.go
+++ models/funcss.go
@@ -1,6 +1,15 @@
 package models
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
 type FunCSS struct {
+	UUID   uuid.UUID
 	ID     int64
 	CSSHex string
 	Name   string
@@ -9,7 +18,26 @@ type FunCSS struct {
 type FunCSSDB interface {
 	Page(page int, limit int, name string) ([]*FunCSS, error)
 
+	ByID(id int) (*FunCSS, error)
+
 	Create(funCSS *FunCSS) error
 	Update(funCSS *FunCSS) error
 	Delete(funCSS *FunCSS) error
+}
+
+type funCSSPGX struct {
+	pool pgxpool.Pool
+}
+
+func (fc *funCSSPGX) Page(page int, limit int, name string) ([]*FunCSS, error) {
+	return nil, fmt.Errorf("Not implemented yet")
+}
+
+func (fc *funCSSPGX) Create(FunCSS) error {
+	_, err := fc.pool.Exec(context.Background(), "")
+	return err
+}
+
+func (fc *funCSSPGX) ByID(id int) (*FunCSS, error) {
+
 }
