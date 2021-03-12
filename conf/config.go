7f14303faa06586755927dd88e34880301ac7b7b
daniel-z-johnson <danzaljohnson@gmail.com>
2021-03-11 23:19:22 -0700

    Added some config coding

--- conf/config.go
+++ conf/config.go
@@ -1,4 +1,29 @@
 package conf
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type FunCSSConf struct {
+	DB struct {
+		Host     string `json:"host"`
+		Port     int    `json:"port"`
+		Name     string `json:"Name"`
+		User     string `json:"user"`
+		Password string `json:"password"`
+	} `json:"db"`
+}
+
+func LoadFunCSSConf(file string) (*FunCSSConf, error) {
+	f1, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	funCSSConf := &FunCSSConf{}
+	err = json.NewDecoder(f1).Decode(funCSSConf)
+	if err != nil {
+		return nil, err
+	}
+	return funCSSConf, nil
 }
--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/daniel-z-johnson/funcss/conf"
 	"github.com/daniel-z-johnson/funcss/models"
 )
 
 func main() {
-	fmt.Println("Starting funcss")
+	confFile := flag.String("conf", "config.json", "The location of the config file")
+	conf, err := conf.LoadFunCSSConf(*confFile)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		panic(err)
+	}
+	fmt.Println(conf)
+	fmt.Println("Starting funCSS")
 	badAss, _ := models.FirstBadAssPage()
 	fmt.Println(badAss)
 }
